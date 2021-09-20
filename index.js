669a61ce328dea901ae8c025a3e541ec2c2bb2d7
Erik <elaktrato@gmail.com>
2021-09-20 19:44:00 +0200

    I have barely an idea of what I have just done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send(`
+<h1>Adopt a pet!</h1>
+<p>Browse through the links below to find your new furry friend:</p>
+<ul>
+    <li>Dogs</li>
+    <li>Cats</li>
+    <li>Rabbits</li>
+</ul>
+`));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
